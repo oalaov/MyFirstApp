b0311b86fa5036c5c0e45a77368ff8f9087bdf8b
oalaov <magamedkurbanov000@gmail.com>
2025-09-26 18:03:40 +0300

    test release 3. Adder debug login button and greetings text.

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,23 +4,12 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myfirstapp.ui.theme.BlankFragment
-import com.example.myfirstapp.ui.theme.MyfirstappTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("MissingInflatedId")
@@ -28,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val debugLogin = findViewById<Switch>(R.id.switch2)
         val button = findViewById<Button>(R.id.enterButton)
         val entemail = findViewById<EditText>(R.id.emailAddress)
         val entpassword = findViewById<EditText>(R.id.textPassword)
@@ -36,9 +26,10 @@ class MainActivity : ComponentActivity() {
             val password = entpassword.text.toString()
             val truemail = "magamed@gmail.com"
             val truepass = "123123"
-            if (mail == truemail && password == truepass) {
+            if (debugLogin.isChecked || mail == truemail && password == truepass) {
                 Toast.makeText(this, "Вы вошли в систему", Toast.LENGTH_LONG).show()
                 val nextActivity = Intent(this, MainActivity2::class.java)
+                intent.putExtra("userNm", mail)
                 startActivity(nextActivity)
 
             } else {
@@ -53,6 +44,8 @@ class MainActivity : ComponentActivity() {
                         clearmail.text.clear()
 
                     }
+
+
 
 
         }
